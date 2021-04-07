1dd61987c0396b25926af55edf9348c0c6d60a5d
Vamost <1780707000@qq.com>
2021-04-07 18:06:37 +0800

    输入json数据处理

--- api/main.py
+++ api/main.py
@@ -1,15 +1,23 @@
 '''
 Author: your name
 Date: 2021-04-07 10:55:44
-LastEditTime: 2021-04-07 11:38:02
+LastEditTime: 2021-04-07 18:06:01
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /testLR/api/main.py
 '''
+
+import  numpy as np
 from typing import List
 
 from fastapi import FastAPI
+from fastapi import Depends
+
+
 from pydantic import BaseModel
+
+from .ml.model import get_model
+
 
 
 class PredictRequest(BaseModel):
@@ -21,6 +29,14 @@ class PredictResponse(BaseModel):
 
 app = FastAPI()
 
-@app.post("/predict", response_model=PredictResponse)
-def predict(input: PredictRequest):
-    return PredictResponse(data=[0.0])
+# @app.post("/predict", response_model=PredictResponse)
+# def predict(input: PredictRequest):
+#     return PredictResponse(data=[0.0])
+
+@app.post("/predict", response_model=PredictResponse):
+def predict(input: PredictRequest, model: Model = Depends(get_model)):
+    X = np.array(input.data)
+    y_pred = model.predict(X)
+    result = PredictResponse(data=y_pred.to_list())
+
+    return result
