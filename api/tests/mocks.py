b072143a1954a1abd0b3a0873f9cbb0823c54840
Vamost <1780707000@qq.com>
2021-04-07 21:13:13 +0800

    使用Mock Object进行测试

--- api/tests/mocks.py
+++ api/tests/mocks.py
@@ -1,8 +1,29 @@
 '''
 Author: your name
 Date: 2021-04-07 18:28:56
-LastEditTime: 2021-04-07 18:28:56
+LastEditTime: 2021-04-07 21:12:59
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /testLR/api/tests/api/mocks.py
 '''
+
+import numpy as np
+
+
+class MockModel:
+    def __init__(self, model_path: str =None):
+        self._model_path = None
+        self._model = None
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        n_instances = len(X)
+        return np.random.rand(n_instances)
+
+    def train(self, X: np.ndarray, y:np.ndarray):
+        return self
+
+    def save(self):
+        pass
+
+    def load(self):
+        return self
