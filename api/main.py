b57667149a3d9af1abd6b979df7bf9b4cdad2ad5
Vamost <1780707000@qq.com>
2021-04-07 11:39:28 +0800

    基础请求响应文件

--- api/main.py
+++ api/main.py
@@ -1,8 +1,26 @@
 '''
 Author: your name
 Date: 2021-04-07 10:55:44
-LastEditTime: 2021-04-07 11:30:10
+LastEditTime: 2021-04-07 11:38:02
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /testLR/api/main.py
 '''
+from typing import List
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class PredictRequest(BaseModel):
+    data: List[List[float]]
+
+
+class PredictResponse(BaseModel):
+    data: List[float]
+
+app = FastAPI()
+
+@app.post("/predict", response_model=PredictResponse)
+def predict(input: PredictRequest):
+    return PredictResponse(data=[0.0])
