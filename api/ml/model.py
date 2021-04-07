b8d46443c55193a703fd81cb77fe221039460054
Vamost <1780707000@qq.com>
2021-04-07 11:46:10 +0800

    模型基础框架

--- api/ml/model.py
+++ api/ml/model.py
@@ -1,8 +1,20 @@
 '''
 Author: your name
 Date: 2021-04-07 10:56:38
-LastEditTime: 2021-04-07 10:56:38
+LastEditTime: 2021-04-07 11:45:56
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /testLR/api/ml/model.py
 '''
+class Model:
+    def train(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def save(self):
+        pass
+
+    def load(self):
+        pass
